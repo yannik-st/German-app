0578623c30b62aef9d932ee3191454b3bd5590e9
yannik <st117697@stud.uni-stuttgart.de>
2021-05-05 18:43:07 +0800

    starting db decks infrastructure

--- german_app/lib/DeckMenu.dart
+++ german_app/lib/DeckMenu.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'db_words.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+Future<List<String>> _loadDecks() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  // if there is no list yet, we initialize it with the lecture deck
+  if (prefs.getStringList('decks') == null){
+    List<String> list = ['Lecture'];
+    prefs.setStringList('decks', list);
+    print('first_time');
+  }
+  List<String> deck_list = prefs.getStringList('decks');
+
+  return deck_list;
+}
 
 class DeckMenu extends StatelessWidget {
   DeckMenu();
-
-  final decks = ['deck1', 'deck2', 'deck3'];
 
   @override
   Widget build(BuildContext context) {
@@ -19,27 +30,37 @@ class DeckMenu extends StatelessWidget {
     ),
     body:  Padding(
       padding: const EdgeInsets.fromLTRB(12, 14, 12, 12),
-      child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16),
-          itemCount: decks.length,
-          itemBuilder: (BuildContext ctx, index) {
-            return Card(
-              color: Colors.deepPurple,
-              child: InkWell(
-                splashColor: Colors.white,
-                onTap: () {
-                  print('Card tapped.');
-                },
-                child: Center(
-                  child: Text(decks.elementAt(index),style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white)),
-                ),
-              ),
-            );
-          }),
+      child: FutureBuilder(
+        builder: (context, content) {
+          if (content.connectionState == ConnectionState.none ||
+              content.hasData == false) {
+            return Container();
+          } else {
+            return GridView.builder(
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16),
+                itemCount: content.data.length,
+                itemBuilder: (BuildContext ctx, index) {
+                  return Card(
+                    color: Colors.deepPurple,
+                    child: InkWell(
+                      splashColor: Colors.white,
+                      onTap: () {
+                        print('Card tapped.');
+                      },
+                      child: Center(
+                        child: Text(content.data.elementAt(index),style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white)),
+                      ),
+                    ),
+                  );
+                });
+          }
+        },
+          future: _loadDecks(),
+      ),
     ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.amber,
